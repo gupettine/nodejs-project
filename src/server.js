446c46f960ab0889d905fa28f5589bcc2d4ef607
gupettine <gupettine@gmail.com>
2024-08-09 13:26:52 -0300

    feature: create and list routes (http methods)

--- src/server.js
+++ src/server.js
@@ -1,6 +1,16 @@
 import http from 'node:http'
 
 const server = http.createServer((req, res) => {
+  const { method, url } = req
+
+  if (method === 'GET' && url === '/users') {
+    return res.end('List of users.')
+  }
+
+  if (method === 'POST' && url === '/users') {
+    return res.end('Create users.')
+  }
+
   return res.end('Hello, World!')
 })
 
